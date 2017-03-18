690c009ed816bccea0b14b44b413f915b4ad3732
Esdras Rodrigues do Carmo <esdrasbrz@gmail.com>
2017-03-18 04:03:27 -0300

    Atualizado para os arquivos corretos

---
+++ code.c
@@ -0,0 +1,19 @@
+int main()
+{
+	//lendo a imagem de um dispositivo
+	Matrix M = read_image();
+
+	//você tem que dar o valor correto às duas variáveis abaixo.
+	//cone_x e cone_y são os pontos x e y, respectivamente, do centro
+	//do retângulo que encobre completamente o cone.
+	int cone_x, cone_y;
+
+	//bônus: dê valor corretamente à variável abaixo
+	float hor_angle = 0.0;
+
+	//----COMPLETE O CÓDIGO AQUI-----
+
+
+
+	//-------------------------------
+}
---
+++ code.py
@@ -0,0 +1,20 @@
+def main():
+    #lendo a imagem de um dispositivo
+    M = read_image()
+
+    #você tem que dar o valor correto às duas variáveis abaixo.
+    #cone_x e cone_y são os pontos x e y, respectivamente, do centro
+    #do retângulo que encobre completamente o cone.
+    cone_x, cone_y = 0, 0
+    
+    #bônus: dê valor corretamente à variável abaixo
+    hor_angle = 0.0
+
+    #----COMPLETE O CÓDIGO AQUI-----
+
+
+
+    #-------------------------------
+    
+if __name__ == "__main__":
+    main()
